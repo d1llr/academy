be67c636781842f4de5eb97b7fae721df0a93e06
“d1llr” <madramov.02@gmail.com>
2023-04-18 16:19:10 +0300

    redux fix 2

--- redux/api/userApi.ts
+++ redux/api/userApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const userApi = createApi({
   reducerPath: 'userApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://localhost:8000/' }),
   endpoints: (builder) => ({
     getUser: builder.query<string, number>({
       query: (limit = 5) => `/users?limit=${limit}`,
